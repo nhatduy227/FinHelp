c80e324de11090f8dc867cba3ee4a963fc614d20
tungtrn <tungt1281@gmail.com>
2022-04-05 14:44:44 -0400

    Finishing theme, navbar, header

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,22 @@
-import React, { useState, createContext, useContext, useEffect } from "react";
-import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import Analytics from "./Pages/Analytics/Analytics";
-import LiveMarket from "./Pages/LiveMarket/LiveMarket";
-import Login from "./Pages/Login/Login";
-import { auth } from "./firebase-config";
-import Grid from "@mui/material/Grid";
-import Header from "./Components/Header/Header";
-import NavBar from "./Components/NavBar/NavBar";
+import './App.css';
+import './App.css';
+
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './Components/Header/Header';
+import NavBar from './Components/NavBar/NavBar';
+import { auth } from './firebase-config';
+import Analytics from './Pages/Analytics/Analytics';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import LiveMarket from './Pages/LiveMarket/LiveMarket';
+import Login from './Pages/Login/Login';
 import Onboarding from "./Pages/Onboarding/Onboarding";
 
 function App() {
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
   const [firstTimeUser, setFirstTimeUser] = useState(true);
+
   auth.onAuthStateChanged((user) => {
     if (user) {
       setIsUserSignedIn(true);
@@ -28,6 +31,20 @@ function App() {
     }
   },[])
 
+  const renderRoute = (
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      <Routes>
+        <Route path="/Analytics" element={<Analytics />} />
+      </Routes>
+      <Routes>
+        <Route path="/LiveMarket" element={<LiveMarket />} />
+      </Routes>
+    </div>
+  )
+
   if (isUserSignedIn === true) {
     if (firstTimeUser === true) {
       return (<Router>
@@ -38,32 +55,11 @@ function App() {
     }
     return (
       <Router>
-        <div className="root">
-          <Grid container direction="column">
-            <Grid item className="header">
-              <Header />
-            </Grid>
-            <Grid item className="content">
-              <Grid container>
-                <Grid item sm={4} md={3} lg={2}>
-                  <NavBar />
-                </Grid>
-                <Grid item sm={8} md={9} lg={10}>
-                  <div className="container">
-                    <Routes>
-                      <Route path="/" element={<Dashboard />} />
-                    </Routes>
-                    <Routes>
-                      <Route path="/Analytics" element={<Analytics />} />
-                    </Routes>
-                    <Routes>
-                      <Route path="/LiveMarket" element={<LiveMarket />} />
-                    </Routes>
-                  </div>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
+        <div>
+          <Header />
+          <NavBar>
+            {renderRoute}
+          </NavBar>
         </div>
       </Router>
     );
