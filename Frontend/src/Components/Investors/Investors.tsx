a58e81e690432ce87ea0e97f596b46dc88d8965e
ngducanh126 <ngducanh126@gmail.com>
2022-04-07 09:11:03 -0400

    dashboard

--- Frontend/src/Components/Investors/Investors.tsx
+++ Frontend/src/Components/Investors/Investors.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import "./Investors.css"
 
 export const Investors=()=>{
+    let investors: { name: string; comp: string; contact: string; }[] = []
+    investors=[
+        {
+            name:'sam',
+            comp:'google',
+            contact:'sam@gmail.com'
+        }
+    ]
+    for (let i of investors){
+        <div className="card">{i.name}</div>
+    }
+    
     return(
         <div className="area">
 
@@ -12,7 +24,10 @@ export const Investors=()=>{
             </div>
 
             <div className="card_area">
-                <div className="card"></div>
+                
+                <div className="card">
+
+                </div>
                 <div className="card"></div>
                 <div className="card"></div>
                 <div className="card"></div>
