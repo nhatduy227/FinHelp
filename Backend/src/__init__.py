3cf5e613eea703a2c5401ab9dee9d9bb3b7c1b92
Nam Dao <59298563+nhd36@users.noreply.github.com>
2022-04-08 11:37:45 -0400

    Add Flask CORS
    config CORS for APIs

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 import src.services as services
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.register_blueprint(services.stock_news_service)
-app.register_blueprint(services.stock_price_service)
+# Config CORS
+app.config["CORS_HEADERS"] = ["Content-Type"]
+app.config["CORS_ORIGIN"] = "*"
+app.config["CORS_SUPPORT_CREDENTIALS"] = True
 
-  
+cors = CORS(app=app, resource={r"/*": {"origins": "*"}})
+
+app.register_blueprint(services.stock_news_service)
+app.register_blueprint(services.stock_quote_service)
+
