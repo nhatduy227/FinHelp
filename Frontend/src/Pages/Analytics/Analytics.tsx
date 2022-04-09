d071bbc347d2757039770a82bec2a2ab77e24189
Andrew Ng <ngducanh126@gmail.com>
2022-04-09 00:46:55 -0400

    creating report State and companies State

--- Frontend/src/Pages/Analytics/Analytics.tsx
+++ Frontend/src/Pages/Analytics/Analytics.tsx
@@ -4,6 +4,25 @@ import { NewsCard } from "../Dashboard/Dashboard";
 import Card from '@mui/material/Card';
 import { SafeAnchor } from "react-bootstrap";
 import { useState,useEffect } from "react";
+ 
+// const companies=[
+//   {name:'Tesla',
+//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+//   },
+//   {name:'Apple',
+//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+//   },
+//   {name:'Google',
+//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+//   },
+//   {name:'Netflix',
+//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+//   },
+//   {name:'Facebook',
+//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+//   },
+// ]
+
 
 const news=[
   {
@@ -18,9 +37,27 @@ const news=[
 
 function Analytics() {
   const [report,useReport]=useState('')
+  const [companies,useCompanies]=useState<Array<{name:string,logo:string}>>([])
 
   useEffect(()=>{
     useReport('Apple has made $1.6B in their profit this quarter ')
+    useCompanies([
+      {name:'Tesla',
+       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+      },
+      {name:'Apple',
+       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+      },
+      {name:'Google',
+       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+      },
+      {name:'Netflix',
+       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+      },
+      {name:'Facebook',
+       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
+      },
+    ])
   })
 
   return (
@@ -47,14 +84,21 @@ function Analytics() {
       </div>
 
       <div className='related-report'>
+
         <div className="title">
           Related Financial Reports
         </div>
-        <Card  sx={{p: 2,mr: 5 }} className='related-report-card'></Card>
-        <Card  sx={{p: 2,mr: 5 }}  className='related-report-card'></Card>
-        <Card  sx={{p: 2,mr: 5 }} className='related-report-card'></Card>
-        <Card  sx={{p: 2,mr: 5 }} className='related-report-card'></Card>
-        <Card  sx={{p: 2,mr: 5 }} className='related-report-card'></Card>
+        {companies.map((item)=>{
+          return (
+            <Card  sx={{p: 2,mr: 5 }} className='related-report-card'>
+            <img src={item.logo} />
+            <div className="card-title">{item.name}</div>
+            <div className="date">2022</div>
+            <div className="viewMore">View More</div>
+            </Card>
+          )
+        })}
+        
       </div>
 
     </div>
