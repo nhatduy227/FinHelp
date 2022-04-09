f06d78a3eca8361fc77b833172719339371692f5
tungtrn <tungt1281@gmail.com>
2022-04-09 10:00:34 -0400

    Push financial report

--- Frontend/src/Pages/Analytics/Analytics.tsx
+++ Frontend/src/Pages/Analytics/Analytics.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import './Analytics.css'
-import { NewsCard } from "../Dashboard/Dashboard";
 import Card from '@mui/material/Card';
+import SearchIcon from '@mui/icons-material/Search';
 import { SafeAnchor } from "react-bootstrap";
-import { useState,useEffect } from "react";
- 
-// const companies=[
-//   {name:'Tesla',
-//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-//   },
-//   {name:'Apple',
-//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-//   },
-//   {name:'Google',
-//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-//   },
-//   {name:'Netflix',
-//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-//   },
-//   {name:'Facebook',
-//    logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-//   },
-// ]
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import { useState, useEffect } from "react";
+import Autocomplete from '@mui/material/Autocomplete';
+
+import { Companies } from "../../Components/Header/HeaderData";
+import { FinancialReport } from "./AnalyticsData";
+import {Financial} from "../../Models/FinancialReport";
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "rgb(7,45,75, 0.05)",
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  // width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: '50%',
+  },
+}));
 
 
-const news=[
+const news = [
   {
-    headline:'this headline',
-    image:'https://www.marketplace.org/wp-content/uploads/2019/09/stockmarket.jpg?fit=2880%2C1621',
-    summary:'report summary',
+    headline: 'this headline',
+    image: 'https://www.marketplace.org/wp-content/uploads/2019/09/stockmarket.jpg?fit=2880%2C1621',
+    summary: 'report summary',
     datetime: 20,
     source: 'source1',
     url: 'dummy url'
@@ -36,29 +39,23 @@ const news=[
 ]
 
 function Analytics() {
-  const [report,useReport]=useState('')
-  const [companies,useCompanies]=useState<Array<{name:string,logo:string}>>([])
+  const [report, setReport] = useState('')
+  const [companies, setCompanies] = useState<Array<{ name: string, logo: string }>>([])
 
-  useEffect(()=>{
-    useReport('Apple has made $1.6B in their profit this quarter ')
-    useCompanies([
-      {name:'Tesla',
-       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-      },
-      {name:'Apple',
-       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-      },
-      {name:'Google',
-       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-      },
-      {name:'Netflix',
-       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-      },
-      {name:'Facebook',
-       logo:'https://cdn.mos.cms.futurecdn.net/irRfUAsTUeNEM676Bd6Wyb.jpg'
-      },
-    ])
-  })
+  const [companyName, setCompanyName] = useState('');
+  const [ companyReport, setCompanyReport ] = useState<Financial>();
+
+  const handleSearchChange = (event: React.SyntheticEvent<Element, Event>, searchString: string) => {
+    console.log(event);
+
+    setCompanyName(searchString);
+    if (event.type === 'click') {
+      console.log("Move to new page");
+
+      //To call fetch and assign to company report
+      setCompanyReport(FinancialReport);
+    }
+  }
 
   return (
     <div className="container" style={{ width: '100%' }}>
@@ -68,37 +65,34 @@ function Analytics() {
           Financial Report Analyzer
         </div>
         <p>
-            Enter a Stock Ticker to get a Detail Analysis of the most recent financial repots
+          Enter a Stock Ticker to get a Detail Analysis of the most recent financial repots
         </p>
       </div>
 
-      <div className="searchBar">
-
-      </div>
+      {/* <Box sx={{ alignContent: 'center' }}> */}
+      <Search sx={{ alignItems: 'center' }}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={Companies}
+          getOptionLabel={(option) => option.label}
+          sx={{ mf: 5 }}
+          renderInput={(params) => <TextField {...params} />}
+          popupIcon={<SearchIcon />}
+          forcePopupIcon={true}
+          inputValue={companyName}
+          onInputChange={(event, newInputValue) => {
+            handleSearchChange(event, newInputValue);
+          }}
+        />
+      </Search>
+      {/* </Box> */}
 
       <div className="report-box">
         <div className="report-content">
-          <h3>{report}</h3>
-         
+            {"Index Score: " + companyReport?.score}
+            {"Report summary: " +companyReport?.summary}
         </div>
-      </div>
-
-      <div className='related-report'>
-
-        <div className="title">
-          Related Financial Reports
-        </div>
-        {companies.map((item)=>{
-          return (
-            <Card  sx={{p: 2,mr: 5 }} className='related-report-card'>
-            <img src={item.logo} />
-            <div className="card-title">{item.name}</div>
-            <div className="date">2022</div>
-            <div className="viewMore">View More</div>
-            </Card>
-          )
-        })}
-        
       </div>
 
     </div>
---
+++ Frontend/src/Pages/Analytics/AnalyticsData.tsx
@@ -0,0 +1,4 @@
+export const FinancialReport = {
+    "score": 120,
+    "summary": "This is a good stock. You should buy it, bet your home on it, and do everything for it"
+}
