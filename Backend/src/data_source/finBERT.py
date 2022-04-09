ebda671f634770700dfe9c7f347169dcfe2560b2
Nom Phan <np656@drexel.edu>
2022-04-09 10:35:52 -0400

    push local

--- Backend/src/data_source/finBERT.py
+++ Backend/src/data_source/finBERT.py
@@ -64,9 +64,9 @@ class dense_opt():
 labels = {0: 'neutral', 1: 'positive', 2: 'negative'}
 num_labels = len(labels)
 vocab = "finance-uncased"
-vocab_path = './analyst_tone/vocab'
-pretrained_weights_path = "./analyst_tone/pretrained_weights"  # this is pre-trained FinBERT weights
-fine_tuned_weight_path = "./analyst_tone/fine_tuned.pth"  # this is fine-tuned FinBERT weights
+vocab_path = 'C:/Users/Nom Phan/Desktop/FinHelp/Backend/src/data_source/analyst_tone/vocab'
+pretrained_weights_path = "C:/Users/Nom Phan/Desktop/FinHelp/Backend/src/data_source/analyst_tone/pretrained_weights"  # this is pre-trained FinBERT weights
+fine_tuned_weight_path = "C:/Users/Nom Phan/Desktop/FinHelp/Backend/src/data_source/analyst_tone/fine_tuned.pth"  # this is fine-tuned FinBERT weights
 max_seq_length = 512
 device = 'cuda:0'
 
